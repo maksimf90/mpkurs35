d79e273641e47224fec325bbd3911a5b19a7dc49
maksimf90 <rikunov95@mail.ru>
2023-12-26 22:52:17 +0300

    1

--- app/src/main/java/com/example/mpkurs35/MapActivity.java
+++ app/src/main/java/com/example/mpkurs35/MapActivity.java
@@ -1,41 +1,61 @@
 package com.example.mpkurs35;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    GoogleMap gMap;
-    FrameLayout map;
+// Implement OnMapReadyCallback.
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnPoiClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set the layout file as the content view.
         setContentView(R.layout.activity_map);
 
-        map = findViewById(R.id.map);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        // Get a handle to the fragment and register the callback.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+    }
+    private static final String TAG = MapActivity.class.getSimpleName();
+
+    // Get a handle to the GoogleMap object and display marker.
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setOnPoiClickListener(this);
+
+        boolean success = googleMap.setMapStyle(new MapStyleOptions(getResources()
+                .getString(R.string.style_json)));
+
+        if (!success) {
+            Log.e(TAG, "Style parsing failed.");
+        }
+        LatLng voronezh = new LatLng( 51.66044, 39.2000);
+        googleMap.addMarker(new MarkerOptions()
+                .position(voronezh)
+                .title("Marker in Voronezh"));
+
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        gMap = googleMap;
-
-        LatLng mapRussia = new LatLng(51.660861, 39.200563);
-        this.gMap.addMarker(new MarkerOptions().position(mapRussia).title("Russia"));
-        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapRussia));
+    public void onPoiClick(PointOfInterest poi) {
+        Toast.makeText(this, "Clicked: " +
+                        poi.name + "\nPlace ID:" + poi.placeId +
+                        "\nLatitude:" + poi.latLng.latitude +
+                        " Longitude:" + poi.latLng.longitude,
+                Toast.LENGTH_SHORT).show();
     }
 }
